9021a1fff637a84b70331104917ddd1ade85d6ac
Abdullah108 <abdullahpro44@gmail.com>
2020-08-03 05:19:35 +0500

    Updates

---
+++ precache-manifest.2ae2ff9cca3bebcfaccf0461b0bfcfee.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0ce92667072b342cd636c9b7402ef9",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "eb7d5d712011aef7937c",
+    "url": "/my-app/static/css/2.f88483a6.chunk.css"
+  },
+  {
+    "revision": "790aebf1b5750b970d3f",
+    "url": "/my-app/static/css/main.6c339f4a.chunk.css"
+  },
+  {
+    "revision": "eb7d5d712011aef7937c",
+    "url": "/my-app/static/js/2.810525ef.chunk.js"
+  },
+  {
+    "revision": "a49915fd9088731fedd0f942e7bdf4c6",
+    "url": "/my-app/static/js/2.810525ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "790aebf1b5750b970d3f",
+    "url": "/my-app/static/js/main.9c65e1b6.chunk.js"
+  },
+  {
+    "revision": "b898d921be9694926079",
+    "url": "/my-app/static/js/runtime-main.eed8c4f8.js"
+  },
+  {
+    "revision": "2153a22662df0321480da3afef36e923",
+    "url": "/my-app/static/media/2.2153a226.png"
+  },
+  {
+    "revision": "aff59e1456e9ee38f4030eaef4e9f456",
+    "url": "/my-app/static/media/Address.aff59e14.jpg"
+  },
+  {
+    "revision": "083f0471367bf99ae25e7d35a8057a93",
+    "url": "/my-app/static/media/Akkurat-Mono.083f0471.OTF"
+  },
+  {
+    "revision": "93ff316686eda151387a53e8949fe78f",
+    "url": "/my-app/static/media/Andriod.93ff3166.png"
+  },
+  {
+    "revision": "3b0c67abcda09d78197c46ad6c7db61d",
+    "url": "/my-app/static/media/AppleGradient.3b0c67ab.jpg"
+  },
+  {
+    "revision": "b7633d23e80b94f8210e489fd4f338c9",
+    "url": "/my-app/static/media/ApplePay.b7633d23.png"
+  },
+  {
+    "revision": "aae7e4adbc339465eb539426c4eb3187",
+    "url": "/my-app/static/media/DTokens.aae7e4ad.png"
+  },
+  {
+    "revision": "3f91f445b4d6913f15687807b3a0fc68",
+    "url": "/my-app/static/media/DrukCond-Super-Reduced.3f91f445.ttf"
+  },
+  {
+    "revision": "87e217470841f32ac748a98ceaed0ff9",
+    "url": "/my-app/static/media/GoogleGradient.87e21747.png"
+  },
+  {
+    "revision": "1c826c00f4a11cf5f0142f4410fd026e",
+    "url": "/my-app/static/media/Gradient.1c826c00.jpg"
+  },
+  {
+    "revision": "7c92701b9c364f177cdd9f3fc927bf0f",
+    "url": "/my-app/static/media/GraphikBlack.7c92701b.otf"
+  },
+  {
+    "revision": "4a127c955329def8ac32316e4c6b55a5",
+    "url": "/my-app/static/media/GraphikMedium.4a127c95.otf"
+  },
+  {
+    "revision": "28e540e896ef6ad1271354ea7973ef12",
+    "url": "/my-app/static/media/Laptop.28e540e8.png"
+  },
+  {
+    "revision": "1c4e9e20fa93342e6afcb268a7257586",
+    "url": "/my-app/static/media/MCD.1c4e9e20.png"
+  },
+  {
+    "revision": "e7e00e06ac000107d3684a556c3cbbc3",
+    "url": "/my-app/static/media/P2P.e7e00e06.png"
+  },
+  {
+    "revision": "0755b8eeb660f9ade79a5b251db0e554",
+    "url": "/my-app/static/media/RampsBlog.0755b8ee.png"
+  },
+  {
+    "revision": "23e8dcff14295c067616caf35d067654",
+    "url": "/my-app/static/media/Refresh.23e8dcff.jpg"
+  },
+  {
+    "revision": "dd6296df915f391c29989c19e3e8a94e",
+    "url": "/my-app/static/media/SFMonoMedium.dd6296df.otf"
+  },
+  {
+    "revision": "6f673b3c943f708e18df3976178df6d9",
+    "url": "/my-app/static/media/Tiramisu.6f673b3c.png"
+  },
+  {
+    "revision": "9fb0b05ff6ced36d779473f53645cce4",
+    "url": "/my-app/static/media/Zerofees.9fb0b05f.png"
+  },
+  {
+    "revision": "9bf440f164ffb93002339eee3ec6c5dd",
+    "url": "/my-app/static/media/backgroundcontact.9bf440f1.png"
+  },
+  {
+    "revision": "6389419388e05da09fcb332124640bc8",
+    "url": "/my-app/static/media/bitcoin.63894193.png"
+  },
+  {
+    "revision": "84beca31e51445ca268d3c26d78c1737",
+    "url": "/my-app/static/media/carouselphone.84beca31.png"
+  },
+  {
+    "revision": "d0b2b18da0f3d4de4cfdb4c8901bbf3d",
+    "url": "/my-app/static/media/coinbase.d0b2b18d.svg"
+  },
+  {
+    "revision": "4118770fb20dac1d1018c43e41ea6b4e",
+    "url": "/my-app/static/media/combinator.4118770f.svg"
+  },
+  {
+    "revision": "1c826c00f4a11cf5f0142f4410fd026e",
+    "url": "/my-app/static/media/compoundGradient.1c826c00.jpg"
+  },
+  {
+    "revision": "97e47589c463314a3337f31ed0b11b74",
+    "url": "/my-app/static/media/compoundStack.97e47589.png"
+  },
+  {
+    "revision": "a9205315041bf5dcfcd75c202e1cdfcc",
+    "url": "/my-app/static/media/countmobile.a9205315.png"
+  },
+  {
+    "revision": "be20fc286129eb3e7b6eac96bf693856",
+    "url": "/my-app/static/media/dEqualDollar.be20fc28.jpg"
+  },
+  {
+    "revision": "e6556f88411b389d8a779c14baa3fb46",
+    "url": "/my-app/static/media/dharmaStack.e6556f88.png"
+  },
+  {
+    "revision": "28bd6d66a8573d29ade315f9b8ea0bd8",
+    "url": "/my-app/static/media/ethereum.28bd6d66.png"
+  },
+  {
+    "revision": "64ec75db074d0fa4389808044bb1f139",
+    "url": "/my-app/static/media/ethereumStack.64ec75db.png"
+  },
+  {
+    "revision": "5d446b421955719fa26c8784aa972ba5",
+    "url": "/my-app/static/media/gradient.5d446b42.png"
+  },
+  {
+    "revision": "7385b506790feba93796c7b75b9f6984",
+    "url": "/my-app/static/media/greenvisor.7385b506.svg"
+  },
+  {
+    "revision": "25cce026227471a1166c1f5684766e3d",
+    "url": "/my-app/static/media/iOSBlog.25cce026.png"
+  },
+  {
+    "revision": "bf715388595aa0aaf8c033cf14f7439d",
+    "url": "/my-app/static/media/logo-dark.bf715388.svg"
+  },
+  {
+    "revision": "061967fea116cfd05195d430bc8106dc",
+    "url": "/my-app/static/media/logo-white.061967fe.png"
+  },
+  {
+    "revision": "4709d1e092d478a3c3268338ed18951c",
+    "url": "/my-app/static/media/logo.4709d1e0.png"
+  },
+  {
+    "revision": "5e66d24f8ac1e1c58bc1eb4a273b8b31",
+    "url": "/my-app/static/media/logo.5e66d24f.svg"
+  },
+  {
+    "revision": "9bb1082a368a65395d62205735218a2c",
+    "url": "/my-app/static/media/photo1.9bb1082a.png"
+  },
+  {
+    "revision": "c54a1e3c19b30351810133ed4229f504",
+    "url": "/my-app/static/media/photo2.c54a1e3c.png"
+  },
+  {
+    "revision": "be8a30304851dc8877821a47d6210a38",
+    "url": "/my-app/static/media/photo3.be8a3030.png"
+  },
+  {
+    "revision": "311a1a64fb2fe9dff48bd71ab35b72bb",
+    "url": "/my-app/static/media/polychain.311a1a64.svg"
+  },
+  {
+    "revision": "4e0d3bef333b68b697bf72e56c2e8d6b",
+    "url": "/my-app/static/media/search.4e0d3bef.svg"
+  },
+  {
+    "revision": "90dc70e7c34abdd0c4f8d2ab8c7131cc",
+    "url": "/my-app/static/media/squiggle.90dc70e7.svg"
+  },
+  {
+    "revision": "fdbdc266382bf7175d3c81c9c975e50d",
+    "url": "/my-app/static/media/techStack.fdbdc266.png"
+  }
+]);
